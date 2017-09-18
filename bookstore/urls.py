b6f83dda0d26dacea572f55ca16fc8dbe8d28e39
mlelansky <mlelansky@mail.de>
2017-09-18 23:10:04 +0200

    Add the functionality to vote.

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'bookstore'
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^ratings/$', views.RatingsListView.as_view(), name="ratings"),
+    url(r'^rating/$', views.RatingFormView.as_view(), name="rating")
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.db.models import Count
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+import numpy
 
-from .models import Author, Book
+from .models import Author, Book, Rating
 
 def index(request):
     """
@@ -13,3 +18,50 @@ def index(request):
         "authors_count": authors_count,
         "books_count": books_count
     })
+
+class RatingsListView(ListView):
+    """
+    This class represents the current top ratings.
+    """
+    template_name = "bookstore/ratings.html"
+
+    def get_queryset(self):
+        """
+        This method returns the current ten top ratings.
+        """
+        ratings = Rating.objects.filter(stars__gte=1).annotate(scount=Count('stars'))
+        ratings_converted = self._convert_ratings(ratings)
+        result = [(key, self._calculate_star(value["values"], value["weights"])) for key, value in ratings_converted.items()]
+        result.sort(key=lambda x: x[1])
+        return result[:10]
+
+    def _convert_ratings(self, ratings):
+        """
+        """
+        result = {}
+        for rating in ratings:
+            title = rating.book.title
+            value = rating.scount
+            weight = rating.stars
+
+            if title not in result:
+                result[title] = {}
+            if "values" not in result[title]:
+                result[title]["values"] = []
+            if "weights" not in result[title]:
+                result[title]["weights"] = []
+
+            result[title]["values"].append(value)
+            result[title]["weights"].append(weight)
+        return result
+
+    def _calculate_star(self, values, weights):
+        return numpy.average(values, weights=weights)
+
+class RatingFormView(CreateView):
+    """
+    This class represents the rating of a book.
+    """
+    fields = ["book", "stars"]
+    model = Rating
+    success_url = reverse_lazy("bookstore:ratings")
