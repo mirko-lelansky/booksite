07597b364652b2b9554fde5ed2493873656fe7c5
mlelansky <mlelansky@mail.de>
2017-09-18 22:58:22 +0200

    Register the models for the admin application.

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookstore.models import Author, Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
