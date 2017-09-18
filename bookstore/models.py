b9c248ba1da4dca01c81edbc68ed2f780e557fd6
mlelansky <mlelansky@mail.de>
2017-09-18 22:55:56 +0200

    Add the authors model to the application.

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    This class represents the author of a book.
+    """
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
