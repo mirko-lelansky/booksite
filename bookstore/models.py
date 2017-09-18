98fffc37f0896c927b37449303b927fb50778630
mlelansky <mlelansky@mail.de>
2017-09-18 22:56:38 +0200

    Add the book model to the application.

--- bookstore/models.py
+++ bookstore/models.py
@@ -8,3 +8,12 @@ class Author(models.Model):
     lastname = models.CharField(max_length=200)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField("Died", null=True, blank=True)
+
+class Book(models.Model):
+    """
+    This class represents the books.
+    """
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    isbn = models.CharField("ISBN", max_length=13)
+    summary = models.TextField(max_length=1000)
+    title = models.CharField(max_length=200)
