50812a61d6d6cf56fd7bb207854975a840f51501
mlelansky <mlelansky@mail.de>
2017-09-18 23:08:06 +0200

    Add the default index page.

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book
+
+def index(request):
+    """
+    This is the base index page.
+    """
+    authors_count = Author.objects.count()
+    books_count = Book.objects.count()
+
+    return render(request, "bookstore/index.html", context={
+        "authors_count": authors_count,
+        "books_count": books_count
+    })
