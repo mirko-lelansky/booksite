e344fc3d9e9242b8a9792cd7042dad1fd6eea92b
mlelansky <mlelansky@mail.de>
2017-09-18 23:02:27 +0200

    Introduce a missing model field.

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -4,7 +4,8 @@ from bookstore.models import Author, Book
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+     fields = ["firstname", "lastname", ("date_of_birth", "date_of_death")]
+     list_display = ("firstname", "lastname", "date_of_birth", "date_of_death")
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
--- bookstore/models.py
+++ bookstore/models.py
@@ -9,14 +9,23 @@ class Author(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField("Died", null=True, blank=True)
 
+    def __str__(self):
+        return self.firstname + " " + self.lastname
+
+    class Meta:
+        ordering = ['lastname', 'firstname']
+
 class Book(models.Model):
     """
     This class represents the books.
     """
     author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
     isbn = models.CharField("ISBN", max_length=13)
-    summary = models.TextField(max_length=1000)
+    summary = models.TextField(max_length=1000, null=True, blank=True)
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 class Rating(models.Model):
     """
